164848ca0a6cd3156d82c765b9bade265ef8e1c6
Ganesh Nikam <ganeshni@synerzipune.local>
2023-06-01 17:18:20 +0530

    Title Renamed

--- container/src/components/Header.js
+++ container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Demo Application
           </Typography>
           <Button
             color="primary"
