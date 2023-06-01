fb1546e321415ff343c0c9f790b4f4afdcdf31ce
Ganesh Nikam <ganeshni@synerzipune.local>
2023-06-01 17:58:03 +0530

    Public Path changed

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: 'marketing/index.html'
+    publicPath: 'marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
